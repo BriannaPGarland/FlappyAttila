f498c201b37506f8d28d31d96b68c6b52265d039
Brianna Garland <56358388+BriannaPGarland@users.noreply.github.com>
2020-11-28 16:38:58 -0500

    Update bat_b_ball.vhd
    we have one building showing on the bottom

--- ProjectFiles/bat_b_ball.vhd
+++ ProjectFiles/bat_b_ball.vhd
@@ -19,18 +19,19 @@ ENTITY bat_n_ball IS
 END bat_n_ball;
 
 ARCHITECTURE Behavioral OF bat_n_ball IS
-	SIGNAL gapsize : INTEGER := 60; -- ball size in pixels
+	SIGNAL gapsize : INTEGER := 120; -- ball size in pixels
 	CONSTANT bat_w : INTEGER := 6; -- bat width in pixels
 	CONSTANT bat_h : INTEGER := 6; -- bat height in pixels
-	CONSTANT wall_h : INTEGER := 4; -- thickness of the wall
+	CONSTANT wall_h : INTEGER := 65; -- thickness of the wall
 	SIGNAL score : integer :=0; -- score;+1for each wall passed
 	-- distance ball moves each frame
 	SIGNAL ball_speed : STD_LOGIC_VECTOR (9 DOWNTO 0) := CONV_STD_LOGIC_VECTOR (5, 10);
 	SIGNAL wall_on : STD_LOGIC; -- indicates whether wall is at current pixel position
 	SIGNAL bat_on : STD_LOGIC; -- indicates whether bat at over current pixel position
+	signal building_on : std_logic;
 	SIGNAL game_on : STD_LOGIC := '0'; -- indicates whether ball is in play
 	-- current ball position - intitialized to center of screen
-	SIGNAL gap_x : STD_LOGIC_VECTOR(9 DOWNTO 0) := CONV_STD_LOGIC_VECTOR(320, 10);
+	SIGNAL gap_x : STD_LOGIC_VECTOR(9 DOWNTO 0) := CONV_STD_LOGIC_VECTOR(640, 10);
 	SIGNAL wall_y : STD_LOGIC_VECTOR(9 DOWNTO 0) := CONV_STD_LOGIC_VECTOR(5, 10);-- might need to mess around with the height
 	-- bat vertical position
 	CONSTANT bat_y : STD_LOGIC_VECTOR(9 DOWNTO 0) := CONV_STD_LOGIC_VECTOR(400, 10);
@@ -42,9 +43,9 @@ ARCHITECTURE Behavioral OF bat_n_ball IS
     --signal duck_y : integer := 150; --initial duck y position
     --signal duck_top, duck_bottom, duck_left, duck_right : integer := 0; 
 BEGIN
-	red <= NOT bat_on; -- color setup for red ball and cyan bat on white background
+	red <= NOT bat_on;   -- color setup for red ball and cyan bat on white background	
 	green <= NOT wall_on;
-	blue <= NOT wall_on;
+	blue <= NOT wall_on AND building_on;
 	-- process to draw gap
 	-- set ball_on if current pixel address is covered by ball position
 	balldraw : PROCESS (wall_y, gap_x, pixel_row, pixel_col) IS
@@ -59,6 +60,21 @@ BEGIN
 			wall_on <= '0';
 		END IF;
 	END PROCESS;
+	
+	--process to draw the buildings
+	buldingdraw: PROCESS (wall_y, gap_x, pixel_row, pixel_col) IS
+		VARIABLE vx, vy : STD_LOGIC_VECTOR (9 DOWNTO 0);
+	BEGIN
+        IF ((pixel_row < gap_x - gapsize/2) OR (gap_x > gapsize/2)) AND
+		 pixel_row > gap_x + gapsize/2 AND
+			 pixel_col >= wall_y - wall_h AND
+			 pixel_col <= wall_y + wall_h THEN
+				building_on <= '1';
+		ELSE
+			building_on <= '0';
+		END IF;
+	END PROCESS;
+	
 	--process to draw bat
   --   set bat_on if current pixel address is covered by bat position
 	batdraw : PROCESS (bat_x, pixel_row, pixel_col) IS
@@ -83,7 +99,7 @@ BEGIN
 			WAIT UNTIL rising_edge(v_sync);
 			IF serve = '1' AND game_on = '0' THEN -- test for new serve
 			    score<=0;
-			    gapsize<=60;
+			    gapsize<=120;
 			    ball_speed<=CONV_STD_LOGIC_VECTOR (5, 10);
 				game_on <= '1';
 				wall_y_motion <= (ball_speed); -- set vspeed to (- ball_speed) pixels
@@ -124,16 +140,19 @@ BEGIN
 			 wall_y >= bat_y - bat_h/2 THEN
                 IF (bat_x + bat_w/2) <= (gap_x + gapsize/2) AND
                  (bat_x - bat_w/2) >= (gap_x - gapsize/2) Then
+                      
                      --(bat_y + bat_h/2) >= (wall_y - wall_h) AND
                      --(bat_y - bat_h/2) <= (wall_y + wall_h) THEN
                      --nothing, it's all good   
                 ELSE
                 -- hit the wall you lose
+           
                 game_on <= '0';
                 gap_x <= CONV_STD_LOGIC_VECTOR(320, 10);
-                gapsize<=60;
+                gapsize<=120;
 			    ball_speed<=CONV_STD_LOGIC_VECTOR (5, 10);
 			    wall_y_motion<=ball_speed;
+			    
                 END IF;
             END IF;
 			
